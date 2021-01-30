e550e5b06e078ae2afbc9993759c7a2de588b6f9
NicolaiCaruana <48438005+NicolaiCaruana@users.noreply.github.com>
2021-01-30 14:10:51 +0100

    Patrolling and GameManager added for Part 1

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    Random random = new Random();
+    GameObject graphParent;
+    public GameObject[] AIObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject AI = Resources.Load<GameObject>("Prefabs/AI");
+        AI.GetComponent<customAIMoveScriptGrid>().enabled = false;
+        PopulatePatrollingAI();
+    }
+
+    public void SpawnObstacles()
+    {
+        GameObject Obstacle = Resources.Load<GameObject>("Prefabs/Obstacle");
+        Obstacle.transform.localScale = new Vector3(Random.Range(1, 10), Random.Range(1, 10), 1);
+        Instantiate(Obstacle, new Vector3(Random.Range(-50, 50), Random.Range(-30, 50), 0), Quaternion.identity);
+    }
+
+    public void ReScan()
+    {
+        graphParent = GameObject.Find("AStarGrid");
+        graphParent.GetComponent<AstarPath>().Scan();
+        Debug.Log("RE-SCANNING PATH");
+    }
+
+    public void SpawnAI()
+    {
+        GameObject AI = Resources.Load<GameObject>("Prefabs/AI");
+        Instantiate(AI, new Vector3(Random.Range(-50, 50), Random.Range(-30, 50), 0), Quaternion.identity);
+    }
+
+    public void startAI()
+    {
+        GameObject AI = Resources.Load<GameObject>("Prefabs/AI");
+        AI.GetComponent<customAIMoveScriptGrid>().enabled = true;
+        AIObjects = GameObject.FindGameObjectsWithTag("AI");
+        foreach (GameObject obj in AIObjects)
+        {
+            obj.GetComponent<customAIMoveScriptGrid>().enabled = true;
+
+        }
+    }
+
+    public void PopulatePatrollingAI()
+    {
+        if (SceneManager.GetActiveScene().name == "Scene2")
+        {
+            GameObject PatrollingAIPrefab = Resources.Load<GameObject>("Prefabs/PatrollingAI");
+            GameObject PatrollingAI = Instantiate(PatrollingAIPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            KU4Patrol patrolScript = PatrollingAI.GetComponent<KU4Patrol>();
+            for (int i = 0; i < 10; i++)
+            {
+                string waypointItem = "Waypoint " + (i + 1);
+                Debug.Log(waypointItem);
+                patrolScript.Waypoints.Add(GameObject.Find(waypointItem).transform);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/KU4Patrol.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class KU4Patrol : MonoBehaviour
+{
+    Seeker patrolSeeker;
+    Path patrolPath;
+    GameObject graphParent;
+    public Transform firstWaypoint;
+    public List<Transform> Waypoints = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        patrolSeeker = GetComponent<Seeker>();
+        firstWaypoint = GameObject.Find("Waypoint 1").transform;
+        graphParent = GameObject.Find("AStarGrid");
+        graphParent.GetComponent<AstarPath>().Scan();
+        patrolPath = patrolSeeker.StartPath(transform.position, firstWaypoint.position);
+        StartCoroutine(updateGraph());
+
+        StartCoroutine(moveToWaypoints(this.transform));
+    }
+
+    IEnumerator updateGraph()
+    {
+        while (true)
+        {
+            graphParent.GetComponent<AstarPath>().Scan();
+            yield return null;
+        }
+    }
+
+    IEnumerator moveToWaypoints(Transform t)
+    {
+        while (true)
+        {
+            foreach (Transform w in Waypoints)
+            {
+                List<Vector3> posns = patrolPath.vectorPath;
+                Debug.Log("Positions Count: " + posns.Count);
+
+                for (int counter = 0; counter < posns.Count; counter++)
+                {
+                    if (posns[counter] != null)
+                    {
+                        while (Vector3.Distance(t.position, posns[counter]) >= 0.5f)
+                        {
+                            t.position = Vector3.MoveTowards(t.position, posns[counter], 1f);
+                            patrolPath = patrolSeeker.StartPath(t.position, w.position);
+                            yield return patrolSeeker.IsDone();
+                            posns = patrolPath.vectorPath;
+                            yield return new WaitForSeconds(0.2f);
+                        }
+                        patrolPath = patrolSeeker.StartPath(t.position, w.position);
+                        yield return patrolSeeker.IsDone();
+                        posns = patrolPath.vectorPath;
+                    }
+                }
+            }
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
